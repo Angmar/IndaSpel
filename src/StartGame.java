43861e3b8dd4bcbd6e79806531b9136f02a04cb2
Erik <erik.r.svensson@telia.com>
2015-04-29 17:14:16 +0200

    test commit

---
+++ src/StartGame.java
@@ -0,0 +1,23 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class StartGame extends StateBasedGame{
+
+	public StartGame(String name){
+		super(name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
