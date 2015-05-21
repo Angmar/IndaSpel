10e2af44c0498783782986abab37ad6c1bb83ad5
Teodor <teodor@bedford.st>
2015-05-21 17:26:42 +0200

    La till lite kommentarer
    Det kommer fler sen!

--- src/Builder.java
+++ src/Builder.java
@@ -2,16 +2,40 @@ import java.util.ArrayList;
 
 public interface Builder {
 
+	/**
+	 * Return the names of the build options
+	 * @return
+	 */
 	public ArrayList<String> getBuildOptions();
 
+	/**
+	 * Return the build progress
+	 * @return
+	 */
 	public int getProgress();
 
+	/**
+	 * Add the opt parameter to the build queue
+	 * @param opt
+	 */
 	public void queueSpawn(int opt);
 
+	/**
+	 * Return the build queue
+	 * @return
+	 */
 	public ArrayList<Integer> getBuildQueue();
 
+	/**
+	 * Return the build times for the different build options
+	 * @return
+	 */
 	public int[] getBuildTime();
 
+	/**
+	 * Return the build costs for the different build options
+	 * @return
+	 */
 	public int[] getBuildCosts();
 
 }
--- src/Building.java
+++ src/Building.java
@@ -1,8 +1,4 @@
-import org.lwjgl.util.vector.Vector2f;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.StateBasedGame;
 
 public abstract class Building extends GameObject {
 
